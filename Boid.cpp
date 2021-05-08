115ab0e68dc59dc5c1b36a0c63e95a29c9ce7084
charlie572 <barehamcharlie@gmail.com>
2021-05-08 13:07:52 +0100

    Make boids turn towards velocity

--- Boid.cpp
+++ Boid.cpp
@@ -1,4 +1,7 @@
+#define _USE_MATH_DEFINES
 #include "Boid.hpp"
+#include <math.h>
+#include "functions.hpp"
 
 
 Boid::Boid(Vector2f pos, Vector2f vel, float size, Color color) 
@@ -16,7 +19,15 @@ void Boid::draw(RenderWindow& window) {
 }
 
 void Boid::update(Time dt) {
+	// move
 	shape.move(velocity * dt.asSeconds());
+
+	// turn towards velocity
+	double angle = atan(-velocity.x / velocity.y);
+	if (velocity.y < 0) {
+		angle = M_PI + angle;
+	}
+	shape.setRotation(degrees(angle));
 }
 
 
